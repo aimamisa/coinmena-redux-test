46f95992313481a8756529d0686df4a5bcebe74d
abdulrazaq24 <imamabdulrazaq@gmail.com>
2021-05-09 21:00:15 +0100

    Added styles

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "./Spinner.css";
+// import "./Spinner.css";
+import "./loader-pulse.css"
 
 const Spinner = () => {
-  return <div className="loader">Loading...</div>;
+  return (
+    <div className="loader">
+      <div className="loading-animation"></div>
+    </div>
+  );
 };
 
 export default Spinner;
